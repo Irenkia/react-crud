1341fe37bef849e559976f158c3cdab24a5ebe73
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-13 16:04:34 +0200

    changed file TutorialList and doing search, setActiveTutorial, deleteAll

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Router>
       <div className="container mt-3">
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <nav className="navbar navbar-expand navbar-dark bg-dark  mb-3">
           <Link to={"/tutorials"} className="navbar-brand">
             MyApp
           </Link>
