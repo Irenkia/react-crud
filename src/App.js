7a4e1af0199e22db5823c03bd429c315c32f63a9
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-14 10:34:59 +0200

    deploy react router app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Tutorial from "./components/tutorial";
 import AddTutorial from "./components/AddTutorial";
 import TutorialsList from "./components/TutorialsList";
